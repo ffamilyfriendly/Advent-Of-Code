7081993957948432298fab7d6f45a58ddbf1f203
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2021-12-02 19:21:45 +0100

    day two beep beep

--- day_2/challange_1/main.cpp
+++ day_2/challange_1/main.cpp
@@ -1,4 +1,35 @@
+/*
+*   Right, day two. This also means music recomendation number two!
+*   last time (https://github.com/ffamilyfriendly/Advent-Of-Code/blob/main/day_1/challange_1/main.cpp) 
+*   it was mischief brew and I intend to keep it that way today too!
+*   
+*   Todays song: When It Rains
+*   https://open.spotify.com/track/75oC69tZF2ZFx6hErzresj
+*/
+
+#include <iostream>
+#include <fstream>
 
 int main(int argc,  char** argv) {
-    
+    std::fstream file;
+    file.open("input.txt", std::ios::in);
+    if(!file || !file.is_open()) {
+        printf("skill issue. File not found\n");
+        return 1;
+    }
+
+    int x = 0;
+    int y = 0;
+    std::string line;
+    while(getline(file, line)) {
+        int sPos = line.find(' ');
+        std::string nr = line.substr(sPos);
+        if(line[0] == 'f')
+            x += std::stoi(nr);
+        else if(line[0] == 'd')
+            y += std::stoi(nr);
+        else y -= std::stoi(nr);
+    }
+
+    printf("location is %d %d! Answer: %d\n", x, y, x * y);
 }
--- day_2/challange_2/main.cpp
+++ day_2/challange_2/main.cpp
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <fstream>
 
 int main(int argc,  char** argv) {
-    
+    std::fstream file;
+    file.open("input.txt", std::ios::in);
+    if(!file || !file.is_open()) {
+        printf("skill issue. File not found\n");
+        return 1;
+    }
+
+    int x = 0;
+    int y = 0;
+    int aim = 0;
+    std::string line;
+    while(getline(file, line)) {
+        int sPos = line.find(' ');
+        std::string nr = line.substr(sPos);
+        int arg = std::stoi(nr);
+        if(line[0] == 'f') {
+            x += arg;
+            y += aim * arg;
+        }
+        else if(line[0] == 'd')
+            aim += arg;
+        else aim -= arg;
+    }
+
+    printf("location is %d %d! Answer: %d\n", x, y, x * y);
 }
