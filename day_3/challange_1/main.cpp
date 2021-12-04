aadc4893dc6652976ca05a06af04901e697fefdd
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2021-12-05 00:15:57 +0100

    I am dumb but day 3

--- day_3/challange_1/main.cpp
+++ day_3/challange_1/main.cpp
@@ -1,4 +1,54 @@
+/*
+*   I'm doing this one day later, was busy yesterday.
+*   Todays song is from a different artist than yesterday (https://github.com/ffamilyfriendly/Advent-Of-Code/blob/main/day_2/challange_1/main.cpp) just to spice it up a bit
+*   
+*   Todays song: Welcome to Slime City by Harley Poe
+*   https://open.spotify.com/track/3C8TtSfNGMMihlumnySXkT
+*/
+
+#include <iostream>
+#include <fstream>
+
+struct intTupple {
+    int zero;
+    int one;
+};
 
 int main(int argc,  char** argv) {
-    
+    std::fstream file;
+    file.open("input.txt", std::ios::in);
+    if(!file || !file.is_open()) {
+        printf("skill issue. File not found\n");
+        return 1;
+    }
+
+    intTupple rates[12];
+    std::string line;
+    while(getline(file, line)) {
+        for(int i = 0; i < line.length(); i++) {
+            if(line[i] == '0')
+                rates[i].zero++;
+            else
+                rates[i].one++;
+        }
+    }
+
+    // actually treating the bin number as a string is likely going to give me issues in part 2
+    std::string gamma;
+    std::string epsilon;
+
+    for(int i = 0; i < 12; i++) {
+        if(rates[i].one > rates[i].zero) {
+            gamma += '1';
+            epsilon += '0';
+        } else {
+            epsilon += '1';
+            gamma += '0';
+        }
+    }
+
+    int decGamma = std::stoi(gamma, 0, 2);
+    int decEpsilon = std::stoi(epsilon, 0, 2);
+
+    printf("gamma: %s (%d) epsilon: %s (%d)\nAnswer: %d\n", gamma.c_str(), decGamma , epsilon.c_str(), decEpsilon, decGamma * decEpsilon);
 }
--- day_3/challange_2/main.cpp
+++ day_3/challange_2/main.cpp
@@ -1,4 +1,94 @@
+// right, this probably calls for a recursive function for each bit field. Let's do that
+// actually scratch above. By simply keeping the intTupple array we already know what number will be most common in each bitpos
+// Scratch above too. This challange took me way too long because I'm dumb and didnt read. My problem was keeping the values for the most
+// common number from the first itteration, which became wrong after the first itteration as elements were removed and thus the numbers became
+// wrong. Lesson to next time: read the fucking challange, dumb swede
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
+
+struct intTupple {
+    int zero;
+    int one;
+};
+
+#define WIDTH 12
+
+
+std::array<intTupple, WIDTH> getMostCommon(std::vector<std::string> v) {
+    std::array<intTupple, WIDTH> rv;
+    for(int i = 0; i < WIDTH; i++) {
+        intTupple a;
+        a.one = 0;
+        a.zero = 0;
+        rv[i] = a;
+    }
+    intTupple rates[WIDTH];
+    for(std::string line: v) {
+        for(int i = 0; i < line.length(); i++) {
+            if(line[i] == '0')
+                rv[i].zero++;
+            else
+                rv[i].one++;
+        }
+    }
+
+    return rv;
+}
+
+int doMagicThing(std::vector<std::string> vec, bool mode, int pos = 0) {
+    std::vector<std::string> remaining;
+
+
+    std::array<intTupple, WIDTH> rates = getMostCommon(vec);
+
+
+    //std::cout << "###" << rates[pos].one << "|" << rates[pos].zero << std::endl;
+    for(std::string l: vec) {
+        char hasToMatch = '0';
+        if(!mode) {
+            if((rates[pos].one >= rates[pos].zero)) hasToMatch = '1';
+        } else {
+            if(rates[pos].one < rates[pos].zero) hasToMatch = '1';
+        }
+
+        //std::cout << mode << "|[" << pos << "] " << l << "(" << hasToMatch << ")" << std::endl;
+        if(mode && pos == 8) printf("%s\n", l.c_str());
+        if(l[pos] == hasToMatch) {
+            remaining.push_back(l);
+            if(pos == WIDTH - 1) printf("found %s!!!!\n", l.c_str());
+        };
+    }
+
+    if(remaining.size() == 0) {
+        printf("failed. Mode: %d. Pos: %d\n", mode, pos);
+        return -1;
+    }
+
+    if(remaining.size() == 1) {
+        //std::cout << "[" << mode << "]" << "found" << remaining.at(0) << std::endl;;
+        return std::stoi(remaining.at(0), 0, 2);
+    }
+    else return doMagicThing(remaining, mode, pos+1);
+};
 
 int main(int argc,  char** argv) {
-    
+    std::fstream file;
+    file.open("input.txt", std::ios::in);
+    if(!file || !file.is_open()) {
+        printf("skill issue. File not found\n");
+        return 1;
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+    while(getline(file, line))
+        lines.push_back(line);
+
+    int co = doMagicThing(lines, false);
+    int oxy = doMagicThing(lines, true);
+
+    printf("co: %d, oxy: %d\nAnswer: %d\n", co, oxy, co * oxy);
 }
